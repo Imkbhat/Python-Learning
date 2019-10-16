1461ff7f34e259838d09135352b58ec8bc54fad2
Kiran B S <kbs71190@gmail.com>
2019-10-16 17:40:35 +0530

    Add files via upload

---
+++ DataBaseConnectivity.py
@@ -0,0 +1,43 @@
+from array import array
+from __builtin__ import set
+print("Hello World")
+array = [100,101,102,103,104]
+for i in array :
+    print i
+    
+    
+def addition(n):
+    return n+n
+
+#result = map(addition, numbers)
+#print result
+
+l = ['kiran','bhat']
+test = list(map(list, l))
+print test
+
+
+def iAmFrom(country = 'India'):
+    print("I am from "+country)
+
+
+iAmFrom('Australia');
+iAmFrom()
+
+#####################JDBC Connectivity###########################################
+#Need to install psycopg2 in ubuntu
+import psycopg2
+
+conn = psycopg2.connect(database = "nmc", user="postgres", password="postgres", host = "127.0.0.1", port = "5432")
+
+print "Opened database successfully"
+
+cur = conn.cursor()
+cur.execute('''SET search_path to nmc''')
+cur.execute('''SELECT * FROM patient_registration LIMIT 1''')
+rows = cur.fetchall()
+
+for row in rows :
+    print "Mrno : "+row[0]
+
+conn.close()
